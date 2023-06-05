dd7b1779bf48e29bf688d52d2838d3b1b8415595
Naoki <Sgt7xNaoking@outlook.es>
2023-06-04 21:17:22 -0500

    commit initial

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+var icono = document.querySelector("#icono");
+
+window.onscroll = function() {
+    var scroll = document.documentElement.scrollTop;
+    
+    if (scroll > 500){
+        icono.style.transform = "scale(1)"
+    }
+    else if(scroll < 500){
+        icono.style.transform = "scale(0)"
+    }
+}
+
+window.onload = () => {
+    let loading = document.querySelector('.loader-container')
+    setTimeout(() => {
+    loading.style.visibility = 'hidden' ;
+    loading.style.opacity = '0' ;
+    },8000)
+}
+
